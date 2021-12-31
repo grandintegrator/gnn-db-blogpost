95c7f5db64a56d5af4b2a717ce6369e45b8d18a3
grandintegrator <grandintegrator>
2021-12-31 01:58:00 +0000

    Added further background

--- post/3. Implementation.py
+++ post/3. Implementation.py
@@ -4,6 +4,10 @@
 
 # COMMAND ----------
 
+
+
+# COMMAND ----------
+
 # MAGIC %md
 # MAGIC <img src="files/ajmal_aziz/gnn-blog/IMG_4306FC26B448_1.jpeg" alt="graph-structured-data" width="1000">
 # MAGIC <figcaption align="center"><b>Fig.1 Language and images contain implicit structure allowing for convolutions. How do we generalise convolutions across arbitrary structures? </b></figcaption>
